c6461e1b71187ecfb922ab429e9e262e6dc0f3d7
Vivek Kumar <Vivekkumarmndl18@gmail.com>
2024-02-03 09:29:33 +0530

    added active or offline status

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,12 +2,16 @@
 
 import { User } from "@prisma/client";
 import Image from "next/image";
+import useActiveList from "../hooks/useActiveList";
 
 interface AvatarProps {
   user?: User;
 }
 
 const Avatar: React.FC<AvatarProps> = ({ user }) => {
+  const { members } = useActiveList();
+  const isActive = members.indexOf(user?.email!) !== -1;
+
   return (
     <div className="relative">
       <div
@@ -28,7 +32,9 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
           fill
         />
       </div>
-      <span className="
+      {isActive && (
+        <span
+          className="
         absolute
         block
         rounded-full
@@ -41,7 +47,9 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
         w-2
         md:h-3
         md:w-3
-      " />
+      "
+        />
+      )}
     </div>
   );
 };
